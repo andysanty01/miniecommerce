d462d5cf342aea0431578d7b1862dfc121dc93d6
Anderson Lescano <aslescanob@utn.edu.ec>
2022-01-30 11:26:09 -0500

    Actualizar metodo eliminar

--- controllers/role.controller.js
+++ controllers/role.controller.js
@@ -58,7 +58,7 @@ const putUpdateRoles = async (req, res) => {
 
 // Desactivacion de roles
 const deleteRoles = async (req, res) => {
-  const { rol_id } = req.query;
+  const { rol_id } = req.body;
   const response = await db.query(`update cs_role set rol_state=false where rol_id=$1`, [rol_id,]);
   res.json({
     message: "Rol desactivado con exito",
